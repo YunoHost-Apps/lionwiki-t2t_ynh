bc1db8147b0b191a1054294a42b826a122dce5f5
farvardin <farvardin>
2020-04-06 01:13:43 +0200

    update color scheme script

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,6 +5,8 @@
 #=================================================
 
 # dependencies used by the app
+pkg_dependencies="node-less"
+
 
 #=================================================
 # PERSONAL HELPERS
