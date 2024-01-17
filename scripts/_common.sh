f5119938dd7a485c4a2bacf3b7340a7509f9066c
Félix Piédallu <felix@piedallu.me>
2024-01-17 11:29:50 +0100

    Regroup color code validation code, update upgrade

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,16 @@
 # PERSONAL HELPERS
 #=================================================
 
+_validate_color() {
+    # Remove eventual '#' prefix from color
+    color=${color#"#"}
+    # Check if the format is valid, put #555555 instead
+    if [[ ! "$color" =~ ^[A-Fa-f0-9]{6}$ ]]; then
+        color=555555
+    fi
+    echo "$color"
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
